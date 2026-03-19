c12e1ed297f68739f352128e5ccb7b681d71d254
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-03-19 12:44:07 +0700

    set up spring boot project

---
+++ src/main/java/org/example/sof3041sp26sof3041_01/Sof3041Sp26Sof304101Application.java
@@ -0,0 +1,14 @@
+package org.example.sof3041sp26sof3041_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3041Sp26Sof304101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3041Sp26Sof304101Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/sof3041sp26sof3041_01/Sof3041Sp26Sof304101ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sof3041sp26sof3041_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3041Sp26Sof304101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
